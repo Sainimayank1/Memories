0352ec5a8a147ad4f1f150f7cb3bf73665a07e8f
Mayank <mayanksaini4455@gmail.com>
2022-12-20 18:56:07 +0530

    Add form and post compoment in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {AppBar , Container , Typography , Grow , Grid } from '@material-ui/core'
+import Form from './component/Form/Form.js';
+import memories from './component/images/memories.png'
+import Posts from './component/Posts/Posts';
+import useStyles from './style.js'
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Container maxidth='lg'>
+      <AppBar position='static' className={classes.appBar} color='inherit'>
+        <Typography variant='h2' align='center' className={classes.heading}>Memories</Typography>
+        <img src={memories} alt='memories' height='60' className={classes.image}></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing='3'>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb" ,extended:true}))
+
+
 app.use("/posts",PostRoutes);
 app.get('/',(req,res)=>
 {
